978915c30a009be4a47e85c1942141661b5b19b5
Douglas Rice <rice.doug@gmail.com>
2015-10-03 17:35:58 -0700

    fourth commit

--- main.rb
+++ main.rb
@@ -6,5 +6,36 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => 'dangerbird_rocks'
 
 
+get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect 'new_player'
+  end
+end
 
+get '/new_player' do
+  erb :new_player
+end
 
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  # create a deck and put it in session
+  suits = ['H', 'D', 'S', 'C']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle! # [ ['H', '9'], ['C', 'K']...etc.]
+  
+  # deal cards
+    session[:dealer_cards] = []
+    session[:player_cards] = []
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+
+  erb :game
+end
